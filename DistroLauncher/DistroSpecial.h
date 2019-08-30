bb5fc77ef47da90622dbc49f1e902469b298d14c
Saijin-Naib <19295950+Saijin-Naib@users.noreply.github.com>
2019-08-30 10:21:11 -0400

    Update DistroSpecial.h
    Update to 3.10.2 release.

--- DistroLauncher/DistroSpecial.h
+++ DistroLauncher/DistroSpecial.h
@@ -25,6 +25,6 @@ namespace DistroSpecial
 	const std::wstring commandLineDeleteUSer = L"/usr/sbin/deluser --remove-home ";
 	const std::wstring commandLineQueryUID = L"/usr/bin/id -u ";
 	const PCWSTR commandLineDeleteResolvConf = L"/bin/rm /etc/resolv.conf";
-	const TCHAR UserlandDownloadURL[] = _T("http://dl-cdn.alpinelinux.org/alpine/v3.10/releases/x86_64/alpine-minirootfs-3.10.0-x86_64.tar.gz");
-	const std::wstring UserlandChecksum = L"ec3da7fb5f709a1ce912e6e31fccc5588420c5f1dcecc362c72c989532c1917a";
+	const TCHAR UserlandDownloadURL[] = _T("http://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-minirootfs-3.10.2-x86_64.tar.gz");
+	const std::wstring UserlandChecksum = L"869b1f491d04b712892a4232b2afc24998f05eae4fb114fc6e44f4b1ab67f80c";
 }
