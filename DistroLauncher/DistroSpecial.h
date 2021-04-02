305f4987af879af2823300c1d7448c83fd3b84f4
Klaus Frank <gitlab.ccc@agowa338.de>
2021-04-02 05:38:33 +0200

    Update to 3.14 and change store logo

--- DistroLauncher/DistroSpecial.h
+++ DistroLauncher/DistroSpecial.h
@@ -26,6 +26,6 @@ namespace DistroSpecial
 	const std::wstring commandLineDeleteUSer = L"/usr/sbin/deluser --remove-home ";
 	const std::wstring commandLineQueryUID = L"/usr/bin/id -u ";
 	const PCWSTR commandLineDeleteResolvConf = L"/bin/rm /etc/resolv.conf";
-	const TCHAR UserlandDownloadURL[] = _T("https://dl-cdn.alpinelinux.org/alpine/v3.13/releases/x86_64/alpine-minirootfs-3.13.2-x86_64.tar.gz");
-	const std::wstring UserlandChecksum = L"3c70d90545aaf6f79adab9479031d90a0f07761c3d6be9c9181dd9c492144f80";
+	const TCHAR UserlandDownloadURL[] = _T("https://dl-cdn.alpinelinux.org/alpine/v3.13/releases/x86_64/alpine-minirootfs-3.13.4-x86_64.tar.gz");
+	const std::wstring UserlandChecksum = L"8e21c85c4a5bb406180136aa03f814c56ffc7498ba7886c5ea13fec92d528ead";
 }
