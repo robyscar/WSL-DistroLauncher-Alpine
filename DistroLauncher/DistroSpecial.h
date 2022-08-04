6f9bf73076c96cfce26bf37f4b410258f48934b6
Kao Kittisak <kittisakphiri@gmail.com>
2022-08-04 13:19:50 +0700

    Upgrade to Alpine 3.16.1

--- DistroLauncher/DistroSpecial.h
+++ DistroLauncher/DistroSpecial.h
@@ -27,6 +27,6 @@ namespace DistroSpecial
 	const std::wstring commandLineDeleteUSer = L"/usr/sbin/deluser --remove-home ";
 	const std::wstring commandLineQueryUID = L"/usr/bin/id -u ";
 	const PCWSTR commandLineDeleteResolvConf = L"/bin/rm /etc/resolv.conf";
-	const TCHAR UserlandDownloadURL[] = _T("https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/x86_64/alpine-minirootfs-3.15.0-x86_64.tar.gz");
-	const std::wstring UserlandChecksum = L"ec7ec80a96500f13c189a6125f2dbe8600ef593b87fc4670fe959dc02db727a2";
+	const TCHAR UserlandDownloadURL[] = _T("https://dl-cdn.alpinelinux.org/alpine/v3.16/releases/x86_64/alpine-minirootfs-3.16.1-x86_64.tar.gz");
+	const std::wstring UserlandChecksum = L"e4341c45d0fd233642b34f1a3468065e2af2026b1985938b2b382777c23d4d31";
 }
